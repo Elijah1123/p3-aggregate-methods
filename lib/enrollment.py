284ec5d142404b05eae06f1e2696020db4d2c217
Elijah M <elijah.macharia@student.moringaschool.com>
2025-05-19 10:05:50 +0300

    Aggregate

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,23 +1,36 @@
 from datetime import datetime
+
 class Student:
     def __init__(self, name):
         self.name = name
         self._enrollments = []
+        self._grades = {}  # {Enrollment: grade}
 
-    def enroll(self, course):
+    def enroll(self, course, grade=None):
         if isinstance(course, Course):
             enrollment = Enrollment(self, course)
             self._enrollments.append(enrollment)
             course.add_enrollment(enrollment)
+            if grade is not None:
+                self._grades[enrollment] = grade
         else:
             raise TypeError("course must be an instance of Course")
 
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def course_count(self):
+        return len(self._enrollments)
+
+    def aggregate_average_grade(self):
+        if not self._grades:
+            return 0  #,
+        total = sum(self._grades.values())
+        return total / len(self._grades)
+
+
 class Course:
     def __init__(self, title):
-
         self.title = title
         self._enrollments = []
 
@@ -33,15 +46,23 @@ class Course:
 
 class Enrollment:
     all = []
-    
+
     def __init__(self, student, course):
         if isinstance(student, Student) and isinstance(course, Course):
             self.student = student
             self.course = course
             self._enrollment_date = datetime.now()
-            type(self).all.append(self)
+            Enrollment.all.append(self)
         else:
             raise TypeError("Invalid types for student and/or course")
 
     def get_enrollment_date(self):
         return self._enrollment_date
+
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
